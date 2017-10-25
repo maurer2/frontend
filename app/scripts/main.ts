fd1e8b0b3d615b9a5f0a17cc3127236eed0b2ce0
Martin <mr.mmaurer+work@gmail.com>
2017-10-25 23:00:40 +0200

    fixed base class

--- app/scripts/main.ts
+++ app/scripts/main.ts
@@ -1,6 +1,6 @@
 enum Direction {
-    next = 'left',
-    previous = 'right',
+    next = 'right',
+    previous = 'left',
 }
 
 class BaseGallery {
@@ -12,6 +12,7 @@ class BaseGallery {
     public constructor(private domNode: HTMLElement) {
         const slidesList = this.domNode.querySelectorAll('.gallery_slide') as NodeListOf<HTMLElement>;
         this.slides = Array.prototype.slice.call(slidesList);
+        // this.slides = Array.from(slidesList);
         this.linkNextSlide = this.domNode.querySelector('.gallery_next') as HTMLLinkElement;
         this.linkPreviousSlide = this.domNode.querySelector('.gallery_prev') as HTMLLinkElement;
         this.counter = this.domNode.querySelector('.gallery_counter') as HTMLElement;
@@ -25,14 +26,16 @@ class BaseGallery {
         this.linkPreviousSlide.addEventListener('click', (event: Event) => this.switchSlides(Direction.previous));
     }
 
-    private switchSlides(direction = Direction.next): void {
+    private switchSlides(directionNewSlide = Direction.next): void {
         const activeSlide: HTMLElement = this.slides.find((element: HTMLElement) => {
             return element.classList.contains('gallery_slide--is-current');
         });
         let newSlide: HTMLElement = this.slides[this.slides.indexOf(activeSlide) + 1];
+        let directionCurrentSlide = Direction.previous;
 
-        if (direction === Direction.previous) {
+        if (directionNewSlide === Direction.previous) {
             newSlide = this.slides[this.slides.indexOf(activeSlide) - 1];
+            directionCurrentSlide = Direction.next;
         }
 
         if (newSlide === undefined) {
@@ -40,32 +43,29 @@ class BaseGallery {
         }
 
         // step 1
-        newSlide.classList.add('gallery_slide--is-following');
-        newSlide.classList.add(direction === Direction.next ?
-        'gallery_slide--is-out-of-right-bound' : 'gallery_slide--is-out-of-left-bound');
+        newSlide.classList.add('gallery_slide--is-following', `gallery_slide--is-out-of-${directionNewSlide}-bound`);
 
         // Force reflow to stop browsers combining step 1 and step 2
         this.domNode.getBoundingClientRect();
 
         // step 2
-        activeSlide.classList.add(direction === Direction.next ?
-            'gallery_slide--is-out-of-left-bound' : 'gallery_slide--is-out-of-right-bound');
-        newSlide.classList.remove('gallery_slide--is-out-of-left-bound', 'gallery_slide--is-out-of-right-bound');
+        activeSlide.classList.add(`gallery_slide--is-out-of-${directionCurrentSlide}-bound`);
+        newSlide.classList.remove(`gallery_slide--is-out-of-${directionNewSlide}-bound`);
 
         // step 3
         activeSlide.parentElement.addEventListener('transitionend', (event: TransitionEvent) => {
-            const transitionPropery: string = event.propertyName;
-            const transitionSource: HTMLElement = event.srcElement as HTMLElement;
+            const transitionProperty = event.propertyName;
+            const transitionSource = event.srcElement as HTMLElement;
 
             // ignore other event types and transtion-events from within each slide
-            if (transitionPropery !== 'transform' && this.slides.indexOf(transitionSource) === -1) {
+            if (transitionProperty !== 'transform' && this.slides.indexOf(transitionSource) === -1) {
                 return;
             }
 
             newSlide.classList.add('gallery_slide--is-current');
             newSlide.classList.remove('gallery_slide--is-following');
-            activeSlide.classList.remove('gallery_slide--is-current',
-            'gallery_slide--is-out-of-left-bound', 'gallery_slide--is-out-of-right-bound');
+            activeSlide.classList.remove('gallery_slide--is-current');
+            activeSlide.classList.remove(`gallery_slide--is-out-of-${directionCurrentSlide}-bound`);
 
             // step 4
             this.updateCounter();
@@ -84,4 +84,4 @@ class BaseGallery {
     }
 }
 
-const gallery1 = new BaseGallery(document.querySelector('.gallery--base') as HTMLElement);
+const galleryBase = new BaseGallery(document.querySelector('.gallery--base') as HTMLElement);
