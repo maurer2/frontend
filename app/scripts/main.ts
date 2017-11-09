e949fce882c643a25e1ee17078c82794b86d8f6c
Martin <mr.mmaurer+work@gmail.com>
2017-11-10 00:25:52 +0100

    finished slide in css var

--- app/scripts/main.ts
+++ app/scripts/main.ts
@@ -3,7 +3,6 @@ enum direction {
     previous = 'left',
 }
 
-
 class Slide {
     public constructor(public domElement: HTMLElement, private index: number) {}
 
@@ -36,8 +35,8 @@ class Slide {
 
 class BaseGallery {
     private static directionStrings: Array<direction> = [direction.next, direction.previous];
+    protected slidesContainer: HTMLElement;
     private slides: Array<Slide>;
-    private slidesContainer: HTMLElement;
     private linkNextSlide: HTMLLinkElement;
     private linkPreviousSlide: HTMLLinkElement;
     private navEntries: Array<HTMLElement>;
@@ -254,26 +253,64 @@ class CustomPropertiesGallery extends BaseGallery {
     }
 
     protected switchSlides(activeSlide: Slide, newSlide: Slide, directions: Array<direction>): void {
+        const [directionNewSlide, directionCurrentSlide] = directions;
+
         // check window.CSS support and css-var support
         if ((window as any).CSS && CSS.supports('color', 'var(--testvar)')) {
             // define inherit base style for gallery domElement
             this.domNode.style.setProperty('--directionCurrentSlide', 'translateX(0%)');
-            this.domNode.style.setProperty('--directionNewSlide', 'translateX(100%)');
+            // this.domNode.style.setProperty('--directionNewSlide', 'translateX(100%)');
 
-            // step 1
+            if (directionNewSlide === direction.next) {
+                this.domNode.style.setProperty('--directionNewSlide', 'translateX(100%)');
+            } else {
+                this.domNode.style.setProperty('--directionNewSlide', 'translateX(-100%)');
+            }
+
+            // step 0
             activeSlide.domElement.style.setProperty('transform', 'var(--directionCurrentSlide)');
             newSlide.domElement.style.setProperty('transform', 'var(--directionNewSlide)');
+
+            // step 1
             newSlide.domElement.classList.add('gallery_slide--is-following');
+
+            // step 1.5
+            this.adjustHeight(activeSlide, newSlide);
 
             // Force reflow to stop browsers combining step 1 and step 2
             this.domNode.getBoundingClientRect();
 
             // step 2 - animate
-            this.domNode.style.setProperty('--directionCurrentSlide', 'translateX(-100%)');
+            if (directionNewSlide === direction.next) {
+                this.domNode.style.setProperty('--directionCurrentSlide', 'translateX(-100%)');
+            } else {
+                this.domNode.style.setProperty('--directionCurrentSlide', 'translateX(100%)');
+            }
             this.domNode.style.setProperty('--directionNewSlide', 'translateX(0%)');
 
             // step 3
-            // todo
+            this.slidesContainer.addEventListener('transitionend', (event: TransitionEvent) => {
+                const transitionProperty = event.propertyName;
+                const transitionSource = event.srcElement as HTMLElement;
+
+                // ignore other event types and transtion-events from within each slide
+                if (transitionProperty !== 'transform' && transitionSource !== activeSlide.domElement) {
+                    return;
+                }
+
+                // stop transition-event bubbling up
+                event.stopPropagation();
+
+                // step 4
+                newSlide.domElement.className = 'gallery_slide gallery_slide--is-current';
+                activeSlide.domElement.className = 'gallery_slide';
+
+                // step 5
+                this.updateCounter();
+
+                // step 6
+                this.updateControls();
+            });
         }
     }
 }
