2bc3bfd3351b17a47bb2df5f670652cde6b7ece6
Martin <mr.mmaurer+work@gmail.com>
2018-08-30 00:35:11 +0200

    Webpack Template copying done

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
 const path = require('path');
 const fs = require('fs');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: './src/base/index.js',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist')
@@ -25,6 +26,25 @@ module.exports = {
                     'ts-loader'
                 ]
             }
+            // HTML
+            /*
+            {
+                test: /\.(html)$/,
+                // include: path.join(__dirname, 'src'),
+                use: {
+                    loader: 'html-loader',
+                    options: {
+                        interpolate: true
+                    }
+                }
+            },
+            */
         ]
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: 'src/base/markup.html'
+        })
+    ]
 };
